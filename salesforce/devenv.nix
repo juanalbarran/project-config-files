2f0391a80e16aae25bc8b04e00100fdd3d9f9929
Juan Albarran <juan.albarran@nixs.com>
2026-03-13 15:30:57 +0100

    Salesforce
    Another update of salesforce files

--- salesforce/devenv.nix
+++ salesforce/devenv.nix
@@ -2,8 +2,23 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  salesforce-extension = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "salesforcedx-vscode";
+    publisher = "salesforce";
+    version = "60.4.1";
+    sha256 = "sha256-g+GdVForEFQl8NMeBTky5BUykBXbau7UyuhZAGhpdP8=";
+  };
+
+  custom-vscode = pkgs.vscode-with-extensions.override {
+    vscodeExtensions = with pkgs.vscode-extensions; [
+      salesforce-extension
+      vscjava.vscode-java-pack
+    ];
+  };
+in {
   packages = [
+    custom-vscode
     inputs.sfdx-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
     pkgs.jq
   ];
@@ -18,6 +33,5 @@
     jdk.package = pkgs.jdk21;
   };
 
-  # Export JAVA_HOME for the Neovim Apex Language Server
   env.JAVA_HOME = "${pkgs.jdk21}/lib/openjdk";
 }
