2f51401f496deeeb3bdd2e415092cccf5fc85325
Juan Albarran <juan.albarran@nixs.com>
2026-03-13 14:00:33 +0100

    Salesforce
    Updating the whole configuration

--- salesforce/devenv.nix
+++ salesforce/devenv.nix
@@ -1,17 +1,13 @@
-# devenv.nix
 {
   pkgs,
   inputs,
   ...
 }: {
-  # Core packages required for your Salesforce workflow
   packages = [
-    # Salesforce CLI pulled directly from your flake input
     inputs.sfdx-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
     pkgs.jq
   ];
 
-  # Devenv language modules handle environment setup for you
   languages.javascript = {
     enable = true;
     package = pkgs.nodejs_20;
@@ -19,11 +15,9 @@
 
   languages.java = {
     enable = true;
-    jdk.package = pkgs.jdk17;
+    jdk.package = pkgs.jdk21;
   };
 
-  # Explicitly export JAVA_HOME.
-  # This is highly recommended because Neovim's Apex Language Server
-  # (which is built on top of Eclipse JDTLS) requires it to function properly.
-  env.JAVA_HOME = "${pkgs.jdk17}/lib/openjdk";
+  # Export JAVA_HOME for the Neovim Apex Language Server
+  env.JAVA_HOME = "${pkgs.jdk21}/lib/openjdk";
 }
